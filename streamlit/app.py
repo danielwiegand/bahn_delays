cf8fee0500aa2d33f186ddcf9be8d8fa6c9f50d7
danielwiegand <daniel.a.wiegand@posteo.de>
2021-08-19 14:17:35 +0200

    further work on streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title('My first app')
-
 HOST = "postgres_streams"
 PORT = "5432"
 USER = "postgres"
@@ -25,14 +23,18 @@ result = pd.read_sql(query, con = conn)
 result["n"] = result["n"].astype("int64")
 result["delay"] = result["delay"].fillna(0).astype(np.int64)
 
-result.drop(["stop_id", "timestamp"], axis = 1, inplace = True)
+result.drop(["stop_id", "timestamp", "ct"], axis = 1, inplace = True)
 
-result.set_index("pt", inplace = True)
+result.set_index("pt", inplace = True, drop = False)
 
 result["hour"] = result.index.hour.fillna(0).astype(np.int16)
-
+result["weekday"] = result.index.day_name()
+result["month"] = result.index.month_name()
+result["year"] = result.index.year
 
 # * Define sidebars
+
+st.set_page_config(layout="wide")
 
 journey_type = st.sidebar.multiselect("Filter journey types",
             pd.unique(result["f"]),
@@ -44,13 +46,15 @@ train_type = st.sidebar.multiselect("Filter train types",
             pd.unique(filtered_journeys["c"]),
             default = pd.unique(filtered_journeys["c"]))
 
-filtered_trains = result[result["f"].isin(journey_type) & result["c"].isin(train_type)]
+filtered_all = result[result["f"].isin(journey_type) & result["c"].isin(train_type)]
 
 train_number = st.sidebar.multiselect("Filter train number",
-            pd.unique(filtered_trains["n"]),
-            default = sorted(pd.unique(filtered_trains["n"])))
+            pd.unique(filtered_all["n"]))
 
-filtered_all = result[result["f"].isin(journey_type) & result["c"].isin(train_type) & result["n"].isin(train_number)]
+if train_number:
+    filtered_all = result[result["f"].isin(journey_type) & result["c"].isin(train_type) & result["n"].isin(train_number)]
+else:
+    filtered_all = result[result["f"].isin(journey_type) & result["c"].isin(train_type)]
 
 
 import datetime
@@ -59,24 +63,37 @@ today = datetime.date.today()
 date_from = st.sidebar.date_input("Start date", result.index.min())
 date_to = st.sidebar.date_input("End date", today)
 
+# * Main page
+
+st.title('My first app')
+
+col1, col2 = st.columns(2)
 
 import plotly.express as px
 import plotly.figure_factory as ff
 
+plot_alldays = px.histogram(filtered_all, x = "delay", hover_data = filtered_all.columns)
+col1.plotly_chart(plot_alldays)
 
-hist = px.histogram(filtered_all, x = "delay", hover_data = filtered_all.columns)
-st.plotly_chart(hist)
+plot_hours = px.violin(filtered_all, y = "delay", x = "hour", points = "outliers", hover_data = filtered_all.columns)
+col2.plotly_chart(plot_hours)
 
-violin = px.violin(filtered_all, y = "delay", x = "hour", box = True, points = "all", hover_data = filtered_all.columns)
-st.plotly_chart(violin)
+plot_weekdays = px.violin(filtered_all, y = "delay", x = "weekday", points = "outliers", hover_data = filtered_all.columns, box = True)
+col1.plotly_chart(plot_weekdays)
 
-# import seaborn as sns
-# time_chart = sns.catplot(data = filtered_all, x = "hour", y = "delay", kind = "box")
-# st.pyplot(time_chart)
+plot_months = px.violin(filtered_all, y = "delay", x = "month", points = "outliers", hover_data = filtered_all.columns, box = True)
+col2.plotly_chart(plot_months)
+
+plot_years = px.violin(filtered_all, y = "delay", x = "year", points = "outliers", hover_data = filtered_all.columns, box = True)
+col1.plotly_chart(plot_years)
 
 
 filtered_all
 
+
+#! Zeit filter ermöglichen
+
+#! Verspätunsgcodes reparieren
 
 
 # * CSS styles
@@ -92,18 +109,3 @@ st.markdown(
     ''',
     unsafe_allow_html = True
 )
-
-st.markdown(
-    f'''
-        <style>
-            .element-container:nth-child(3) .st-bv {{
-                height: 200px;
-                overflow: scroll;
-            }}
-        </style>
-    ''',
-    unsafe_allow_html = True
-)
-
-
-# st.button("Update", key=None, help=None, on_click=None, args=None, kwargs=None)
